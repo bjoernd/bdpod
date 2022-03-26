4bacc04c4570656e621c3c8892b5ba634617d7ca
Bjoern Doebel <bjoern.doebel@gmail.com>
2022-03-26 22:33:27 +0100

    Enable logging with flexi_logger
    Signed-off-by: Bjoern Doebel <bjoern.doebel@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+extern crate flexi_logger;
+use log::{debug,info,warn,error};
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    flexi_logger::Logger::try_with_str("debug").unwrap()
+        .log_to_stderr()
+        .format(flexi_logger::colored_detailed_format)
+        .start().unwrap();
+
+    info!("BDPod starting up.");
+
+    info!("The end.");
+    Ok(())
 }
