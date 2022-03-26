009296c7ae3f25f541fbb7834f4c9919514ee276
Bjoern Doebel <bjoern.doebel@gmail.com>
2022-03-26 22:36:41 +0100

    Add a function to kick off OAuth authentication
    Add a function setting up OAuth for Podchaser, which would be required for
    mutating queries later on. Right now we are lacking a programmatic way to
    obtain the resulting auth token because Podchaser doesn't allow me to set
    the API's redirect URI to localhost (which would allow listening for that
    request ourselves and acting on it locally).
    
    Signed-off-by: Bjoern Doebel <bjoern.doebel@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,42 @@
+use oauth2::basic::BasicClient;
+use oauth2::{
+    AuthUrl, ClientId, ClientSecret, CsrfToken, RedirectUrl, Scope, TokenUrl,
+};
 use std::env;
 extern crate flexi_logger;
 use log::{debug,info,warn,error};
+
+#[allow(dead_code)] // TODO remove at some point?
+
+fn generate_oauth2_url(client_id: String, client_secret: String) {
+    let auth_url = AuthUrl::new("https://www.podchaser.com/do-auth".to_string()).expect("Invalid auth URL");
+    let token_url = TokenUrl::new("https://api.podchaser.com/graphql".to_string()).expect("Invalid token URL");
+
+    /* This should redirect to localhost so we could set up a local TCP server to get the redirect request and then parse the auth code.
+       However, podchaser does not support this right now :( */
+    let redirect_url = RedirectUrl::new("https://podchaser.com".to_string()).expect("Invalid redirect URL");
+
+    let client = BasicClient::new(
+        ClientId::new(client_id),
+        Some(ClientSecret::new(client_secret)),
+        auth_url,
+        Some(token_url),
+    )
+    // Set the URL the user will be redirected to after the authorization process.
+    .set_redirect_uri(redirect_url);
+
+    // Generate the full authorization URL. For now, we just request ALL the scopes podchaser supports.
+    let (goto_url, _csrf_token) = client
+        .authorize_url(CsrfToken::new_random)
+        // Set the desired scopes.
+        .add_scope(Scope::new("edit_ratings".to_string()))
+        .add_scope(Scope::new("edit_reviews".to_string()))
+        .add_scope(Scope::new("edit_follows".to_string()))
+        .add_scope(Scope::new("edit_lists".to_string()))
+        .add_scope(Scope::new("edit_listens".to_string()))
+        .url();
+
+    println!("Please open: {}", goto_url);
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -20,6 +56,29 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     debug!("Using client ID:     {}", client_id);
     debug!("Using client secret: {}", client_secret);
+
+    /*
+     * Some mutating queries require OAuth authentication with PodChaser.
+     *
+     * The authorization token is created as follows (https://api-docs.podchaser.com/docs/guides/oauth-guide/):
+     *   - Post an OAUTH request to https://www.podchaser.com/do-auth, add the right scopes you want to request
+     *     authorization for. For convenience, the generate_oauth2_url() function above generates the right URL
+     *     to call.
+     *   - Successful OAuth authorization (via your browser) will redirect you to a redirect URI, which gets an extra
+     *     CODE parameter, which is the authorization token to be used for this app+user combination.
+     *     - Some OAuth2 examples, such as https://github.com/ramosbugs/oauth2-rs/blob/main/examples/wunderlist.rs
+     *       use a TCP server on localhost as the redirect URI, so that they can grab the auth token directly.
+     *     - Unfortunately, Podchaser's API does not allow us to set http://localhost as the redirect URI because
+     *       this fails their URL validation. Hence, we need to redirect elsewhere.
+     *   - Once successfully redirected, check the full URL (not only the part your browser shows you). For Podchaser
+     *     it will contain someting like `/code=XYZ`. XYZ is the authorization token we're looking for and needs to
+     *     be passed to the mutating call.
+     */
+    //generate_oauth2_url(client_id.clone(), client_secret.clone());
+    //let mut token = String::new();
+    //io::stdin().read_line(&mut token)?;
+    // TODO: find a way that doesn't require copy-pasting magic values on the command line
+
 
     info!("The end.");
     Ok(())
